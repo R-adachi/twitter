11d4e9a731653cd205a5f688f4c662e49995d3e2
R-adachi <nisizakakkz@gmail.com>
2019-08-05 07:47:11 +0900

    csv取得モジュール完成

--- get_csv_module.py
+++ get_csv_module.py
@@ -4,10 +4,12 @@ from pprint import pprint
 csv_file=open("../c96_check.csv","r",encoding="ms932",errors="",newline="")
 f=csv.reader(csv_file,delimiter=",",doublequote=True,lineterminator="¥r¥n",quotechar='"',skipinitialspace=True)
 
+header=next(f)
 
 clist=[]
 
 for row in f:
-    clist.append(str(row[3]))
-
+    if(row[2]!=""):
+        clist.append(str(row[2]))
+#pprint(clist)
 return(clist)
