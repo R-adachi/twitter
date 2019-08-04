0d13e8080d31e5e6c8399e4ba5b8457bacb0f227
R-adachi <nisizakakkz@gmail.com>
2019-08-05 08:41:08 +0900

    リストを照合し結果を書き込む 現在はcsvの中身がリストに入っているか

--- get_csv_module.py
+++ get_csv_module.py
@@ -15,3 +15,18 @@ def get():
     #pprint(clist)
 
     return(clist)
+
+def getname():
+    csv_file=open("../c96_check.csv","r",encoding="ms932",errors="",newline="")
+    f=csv.reader(csv_file,delimiter=",",doublequote=True,lineterminator="¥r¥n",quotechar='"',skipinitialspace=True)
+
+    header=next(f)
+
+    clist=[]
+
+    for row in f:
+        if(row[1]!=""):
+            clist.append(str(row[1]))
+    #pprint(clist)
+
+    return(clist)
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from pprint import pprint
 import get_csv_module as getcsv
 import get_list_module as getlist
+import csv
 
 clist=getcsv.get()
+clist_name=getcsv.getname()
 llist=getlist.get()
 
+kekka=[[]]
+
+cnt=0
+
 for i in clist:
-    print(i in llist)
+    check=(i in llist)
+    kekka.append([clist_name[cnt],i,check])
+    cnt+=1
+
+del kekka[0]
+#pprint(status_list[0].text)
+#pprint(status_list)
+
+with open('../c96_kekka.csv', 'w' ,encoding="utf_8_sig") as f:
+    writer = csv.writer(f, lineterminator='\n') # 改行コード（\n）を指定しておく
+    writer.writerows(kekka)     # list（1次元配列）の場合
