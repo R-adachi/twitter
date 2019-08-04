cd25527496db8e060853aebe69a2292b339df3ec
R-adachi <nisizakakkz@gmail.com>
2019-08-05 07:44:26 +0900

    csvリストから取得するモジュール

--- get_csv_module.py
+++ get_csv_module.py
@@ -1,5 +1,13 @@
 import csv
 from pprint import pprint
 
-csv_file=open("../key/key.csv","r",encoding="ms932",errors="",newline="")
+csv_file=open("../c96_check.csv","r",encoding="ms932",errors="",newline="")
 f=csv.reader(csv_file,delimiter=",",doublequote=True,lineterminator="¥r¥n",quotechar='"',skipinitialspace=True)
+
+
+clist=[]
+
+for row in f:
+    clist.append(str(row[3]))
+
+return(clist)
