a9c139d850265737708a9d14f57334d9418f65d7
R-adachi <nisizakakkz@gmail.com>
2019-08-06 08:37:22 +0900

    短期収集用

---
+++ main_s.py
@@ -0,0 +1,40 @@
+from pprint import pprint
+import get_csv_module as getcsv
+import get_list_module as getlist
+import csv
+import get_pic_s_module as getpic
+import time
+
+clist=getcsv.get()
+clist_name=getcsv.getname()
+llist=getlist.get()
+
+kekka=[[]]
+
+cnt=0
+
+for i in clist:
+    check=(i in llist)
+    kekka.append([clist_name[cnt],i,check])
+    cnt+=1
+
+del kekka[0]
+#pprint(status_list[0].text)
+#pprint(status_list)
+
+cnt=0
+start=time.time()
+for i in kekka:
+    if(i[2]):
+        i.append(getpic.get(i[1],i[0]))
+    cnt+=1
+    if(cnt>30):
+        stop=time.time()
+        time.sleep(900-(stop-start))
+        start=time.time()
+        cnt=0
+#kekka[0]=['名前','アカウント','整合性チェック','お品書き']
+
+with open('../c96_kekka.csv', 'w' ,encoding="utf_8_sig") as f:
+    writer = csv.writer(f, lineterminator='\n')
+    writer.writerows(kekka)
