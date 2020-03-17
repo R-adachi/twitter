1f40a3fad40f69edd8b09b19ff6e9653bdd90a31
R-adachi <nisizakakkz@gmail.com>
2020-03-17 18:07:26 +0900

    仮

---
+++ main_circle_notcheck_find.py
@@ -0,0 +1,46 @@
+from pprint import pprint
+import get_csv_module_v2 as getcsv
+import get_list_module as getlist
+import csv
+import circle_check_rt_module as rt
+import circle_check_fv_module as fv
+import circle_check_fv_infvl_module as flfv
+import time
+import make_tlist_module as makelist
+from progressbar import progressbar as pber
+
+
+print('C?:',end="")
+num = int(input())
+c = 'c' + str(num)
+bc = 'c' + str(num-1)
+
+glist=getcsv.get_pre_grade(bc)
+salist = []
+rtlist = []
+rtlist_tmp = []
+fvlist = []
+nfmlist = []
+for i in glist:
+    if(i[4]==('A' or 'S')):
+        salist.append(i[2])
+    elif(i[4]==('B' or '')):
+        fvlist.append(i[2])
+    elif(i[4]==('C' or 'D')):
+        nfmlist.append(i[2])
+
+rtlist_tmp = list(set(salist + getlist.get('ic')))
+for i in rtlist_tmp:
+    if(i not in nfmlist):
+        if(i not in fvlist):
+            rtlist.append(i)
+
+makelist.make(c+'rt',rtlist)
+makelist.make(c+'fv',fvlist)
+
+for i in pber(rtlist):
+    rt.check(i)
+for i in pber(rtlist):
+    fv.check(i)
+for i in pber(fvlist):
+    flfv.check(i)
--- test.py
+++ test.py
@@ -1,16 +1,40 @@
+import tweepy
+import key_module as getkey
 from pprint import pprint
-import get_csv_module_v2 as getcsv
-import get_list_module as getlist
 import csv
-import circle_check_rt_module as rt
-import circle_check_fv_module as fv
-import circle_check_fv_infvl_module as flfv
-import time
-import make_tlist_module as makelist
-from progressbar import progressbar as pber
+import datetime
+
+consumer_key = getkey.get_ckey()
+consumer_secret = getkey.get_ckey_s()
+access_token = getkey.get_atoken()
+access_token_secret = getkey.get_atoken_s()
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+#status_list = api.home_timeline()
 
 
-tmp = list(getlist.get('ic'))
+#c96_list=[[]]
+def get(listname):
+    list=[]
 
+    f = open("../myname.txt", "r")
+    myname = f.read()
+    f.close()
 
-makelist.make('ica',tmp)
+    screen_name=myname #リスト作成者の@~~の~~
+    for member in tweepy.Cursor(api.list_members,slug=listname,owner_screen_name=screen_name).items():
+        #c96_list.append([member.screen_name])
+        list.append(member.screen_name)
+    pprint(datetime.datetime.fromtimestamp(api.rate_limit_status()['resources']['lists']['/lists/members']['reset']))
+    #del c96_list[0]
+    #pprint(status_list[0].text)
+    #pprint(status_list)
+
+    return(list)
+
+if __name__=='__main__':
+    list=get('c98rt')
+    #print(list)
